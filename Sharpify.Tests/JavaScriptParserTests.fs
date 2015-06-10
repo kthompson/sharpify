8008ab602341ab3ceabb027c567a97c771fb5973
Kevin Thompson <kevin.thompson@theautomaters.com>
2015-06-09 19:59:35 -0700

    Initial Commit.

---
+++ Sharpify.Tests/JavaScriptParserTests.fs
@@ -0,0 +1,65 @@
+﻿module JavaScriptParserTests
+
+open Sharpify
+open Xunit
+open FParsec
+
+let test str result =    
+  let parser = JavascriptParser ()  
+  match parser.Parse str with
+  | Success(result, _, _)   -> 
+    printf "Success: %A" result
+    Assert.True true
+  | Failure(errorMsg, _, _) -> 
+    printf "Failure: %s" errorMsg
+    Assert.True false   
+
+[<Fact>]
+let ``Parser Can Parse variable declaration with Number``() =         
+    test @"var x = 5"
+
+[<Fact>]
+let ``Can parse JSON Object``() = 
+  let json = 
+    """{
+        "glossary": {
+            "title": "example glossary"
+        },
+        "value" : 2
+    }"""
+  test json (JObject (Map.empty
+               .Add("glossary", JObject (Map.empty.Add("title", JString "example glossary")))
+               .Add("value", JNumber 2.0)))
+        
+                    
+
+[<Fact>]
+let ``Can parse JSON string``() = 
+  test @"""hello world""" (JString "hello world")
+
+[<Fact>]
+let ``Can parse JSONNullLiteral``() = 
+  test @"null" JNull
+
+[<Fact>]
+let ``Can parse JSONBooleanLiteral``() = 
+  test @"true" (JBool true)
+  test @"false" (JBool false)
+
+[<Fact>]
+let ``Can parse JSONNumber``() = 
+  test @"2.5" (JNumber 2.5)
+  test @"8" (JNumber 8.0)
+  test @"-8.2" (JNumber -8.2)
+  test @"-25" (JNumber -25.0)
+  test @"9e-10" (JNumber 9e-10)
+  test @"8e+45" (JNumber 8e45)
+  test @"8E-5" (JNumber 8e-5)
+  test @"8E+5" (JNumber 8e5)
+
+[<Fact>]
+let ``Can parse JSONArray``() = 
+  test @"[1,2,3,4]" (JList [JNumber 1.0; JNumber 2.0; JNumber 3.0; JNumber 4.0])
+  test @"[""1"",""2"",""3"",""4""]" (JList [JString "1"; JString "2"; JString "3"; JString "4"])
+  test @"[1, 2,3, 4 ]" (JList [JNumber 1.0; JNumber 2.0; JNumber 3.0; JNumber 4.0])
+
---
+++ Sharpify/JavascriptParser.fs
@@ -0,0 +1,66 @@
+﻿namespace Sharpify
+
+open FParsec
+
+type JavascriptParser() = 
+  let ws = spaces
+  let str = pstring 
+
+
+  let stringLiteral =
+    let escape =  anyOf "\"\\/bfnrt"
+                  |>> function
+                      | 'b' -> "\b"
+                      | 'f' -> "\u000C"
+                      | 'n' -> "\n"
+                      | 'r' -> "\r"
+                      | 't' -> "\t"
+                      | c   -> string c // every other char is mapped to itself
+
+    let unicodeEscape =
+        // converts a hex char ([0-9a-fA-F]) to its integer number (0-15)
+        let hex2int c = (int c &&& 15) + (int c >>> 6)*9
+
+        str "u" >>. pipe4 hex hex hex hex (fun h3 h2 h1 h0 ->
+            (hex2int h3)*4096 + (hex2int h2)*256 + (hex2int h1)*16 + hex2int h0
+            |> char |> string
+        )
+
+    let escapedCharSnippet = str "\\" >>. (escape <|> unicodeEscape)
+    let normalCharSnippet  = manySatisfy (fun c -> c <> '"' && c <> '\\')
+
+    between (str "\"") (str "\"")
+            (stringsSepBy normalCharSnippet escapedCharSnippet)
+
+  let listBetweenStrings sOpen sClose pElement f =
+      between (str sOpen) (str sClose)
+              (ws >>. sepBy (pElement .>> ws) (str "," >>. ws) |>> f)
+  
+  let JSONValue, JSONValueRef = createParserForwardedToRef<Json, unit>()
+
+  let JSONArray = listBetweenStrings "[" "]" JSONValue JList
+  let JSONString = stringLiteral |>> JString
+  let JSONNullLiteral = stringReturn "null" JNull
+  let JSONBooleanLiteral = 
+    stringReturn "true" (JBool true)
+    <|> stringReturn "false" (JBool false)  
+  
+  let JSONNumber = pfloat |>> JNumber
+
+  let JSONArray = listBetweenStrings "[" "]" JSONValue JList
+
+  let keyValue = stringLiteral .>>. (ws >>. str ":" >>. ws >>. JSONValue)
+  let JSONObject = listBetweenStrings "{" "}" keyValue (Map.ofList >> JObject)
+
+  do JSONValueRef := JSONNullLiteral
+    <|> JSONBooleanLiteral
+    <|> JSONObject
+    <|> JSONArray
+    <|> JSONString
+    <|> JSONNumber
+
+  let program = 
+    JSONValue .>> eof
+
+  member this.Parse = run program
+      
